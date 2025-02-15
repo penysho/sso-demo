c52140fc9cede4851b6f9e5deda376567116ef5e
penysho <penysho@gmail.com>
2025-02-15 15:14:15 +0900

    feat: cdk frontend追加

--- cdk/lib/frontend.ts
+++ cdk/lib/frontend.ts
@@ -17,6 +17,11 @@ export class FrontendStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: FrontendStackProps) {
     super(scope, id, props);
 
+    const hostedZone = HostedZone.fromHostedZoneAttributes(this, "HostedZone", {
+      hostedZoneId: currentEnvConfig.frontendDomainHostedZoneId,
+      zoneName: "penysho.net",
+    });
+
     const amplifyRole = new iam.Role(this, "AmplifyRole", {
       assumedBy: new iam.ServicePrincipal("amplify.amazonaws.com"),
       managedPolicies: [
@@ -72,6 +77,26 @@ export class FrontendStack extends cdk.Stack {
           source: "/<*>",
           target: "/index.html",
           status: "404-200",
+        },
+      ],
+    });
+
+    const store2Branch = new CfnBranch(this, "Store2Branch", {
+      appId: store2Amplify.attrAppId,
+      branchName: currentEnvConfig.branch,
+      framework: "Next.js - SSR",
+      enableAutoBuild: false,
+      stage: "PRODUCTION",
+    });
+
+    new CfnDomain(this, "Store2Domain", {
+      appId: store2Amplify.attrAppId,
+      domainName: hostedZone.zoneName,
+      enableAutoSubDomain: true,
+      subDomainSettings: [
+        {
+          prefix: "",
+          branchName: store2Branch.branchName,
         },
       ],
     });
@@ -138,29 +163,136 @@ export class FrontendStack extends cdk.Stack {
       stage: "PRODUCTION",
     });
 
-    const store2Branch = new CfnBranch(this, "Store2Branch", {
-      appId: store2Amplify.attrAppId,
+    const authHubAmplify = new CfnApp(this, "AuthHubAmplify", {
+      name: "auth-hub",
+      oauthToken: currentEnvConfig.githubToken,
+      iamServiceRole: amplifyRole.roleArn,
+      repository: "https://github.com/penysho/sso-demo",
+      environmentVariables: [
+        {
+          name: "AMPLIFY_MONOREPO_APP_ROOT",
+          value: "auth-hub",
+        },
+        {
+          name: "NEXT_PUBLIC_API_URL",
+          value: `https://sso-demo-api.pesh-igpjt.com`,
+        },
+      ],
+      buildSpec: BuildSpec.fromObjectToYaml({
+        version: 1,
+        applications: [
+          {
+            appRoot: "auth-hub",
+            frontend: {
+              phases: {
+                preBuild: {
+                  commands: ["nvm install 22.8", "nvm use 22.8", "npm install"],
+                },
+                build: {
+                  commands: ["npm run build"],
+                },
+              },
+              artifacts: {
+                baseDirectory: ".next",
+                files: ["**/*"],
+              },
+              cache: {
+                paths: ["node_modules/**/*"],
+              },
+            },
+          },
+        ],
+      }).toBuildSpec(),
+      platform: "WEB_COMPUTE",
+      customRules: [
+        {
+          source: "/<*>",
+          target: "/index.html",
+          status: "404-200",
+        },
+      ],
+    });
+
+    const authHubBranch = new CfnBranch(this, "AuthHubBranch", {
+      appId: authHubAmplify.attrAppId,
       branchName: currentEnvConfig.branch,
       framework: "Next.js - SSR",
       enableAutoBuild: false,
       stage: "PRODUCTION",
     });
 
-    const hostedZone = HostedZone.fromHostedZoneAttributes(this, "HostedZone", {
-      hostedZoneId: currentEnvConfig.frontendDomainHostedZoneId,
-      zoneName: "penysho.net",
-    });
-
-    new CfnDomain(this, "Store2Domain", {
-      appId: store2Amplify.attrAppId,
-      domainName: hostedZone.zoneName,
+    new CfnDomain(this, "AuthHubDomain", {
+      appId: authHubAmplify.attrAppId,
+      domainName: "auth.penysho.net",
       enableAutoSubDomain: true,
       subDomainSettings: [
         {
           prefix: "",
-          branchName: store2Branch.branchName,
+          branchName: authHubBranch.branchName,
         },
       ],
+    });
+
+    const store3Amplify = new CfnApp(this, "Store3Amplify", {
+      name: "demo-store-3",
+      oauthToken: currentEnvConfig.githubToken,
+      iamServiceRole: amplifyRole.roleArn,
+      environmentVariables: [
+        {
+          name: "AMPLIFY_MONOREPO_APP_ROOT",
+          value: "demo-store-3",
+        },
+        {
+          name: "NEXT_PUBLIC_API_URL",
+          value: `https://sso-demo-api.pesh-igpjt.com`,
+        },
+        {
+          name: "NEXT_PUBLIC_AUTH_HUB_URL",
+          value: `https://auth.penysho.net`,
+        },
+      ],
+      repository: "https://github.com/penysho/sso-demo",
+      buildSpec: BuildSpec.fromObjectToYaml({
+        version: 1,
+        applications: [
+          {
+            appRoot: "demo-store-3",
+            frontend: {
+              phases: {
+                preBuild: {
+                  commands: ["nvm install 22.8", "nvm use 22.8", "npm install"],
+                },
+                build: {
+                  commands: ["npm run build"],
+                },
+              },
+              artifacts: {
+                baseDirectory: ".next",
+                files: ["**/*"],
+              },
+              cache: {
+                paths: ["node_modules/**/*"],
+              },
+            },
+          },
+        ],
+      }).toBuildSpec(),
+      platform: "WEB_COMPUTE",
+      customRules: [
+        {
+          source: "/<*>",
+          target: "/index.html",
+          status: "404-200",
+        },
+      ],
+    });
+
+    new CfnBranch(this, "Store3Branch", {
+      appId: store3Amplify.attrAppId,
+      branchName: currentEnvConfig.branch,
+      framework: "Next.js - SSR",
+      enableAutoBuild: false,
+      stage: "PRODUCTION",
     });
   }
 }
